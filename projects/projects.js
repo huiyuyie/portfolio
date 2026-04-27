9c989f44b67668eabe780ba5fbad930fc18dabc7
huiyuyie <huiyuyie@gmail.com>
2026-04-27 13:22:33 -0700

    Added pie chart for Projects page.
    Added pie chart for Projects page.

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,5 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
@@ -7,3 +9,75 @@ renderProjects(projects, projectsContainer, 'h2');
 
 const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `${projects.length} Projects`;
+
+// Lab 05
+
+// Step 1.3
+// let arcGenerator = d3.arc()
+//   .innerRadius(0)
+//   .outerRadius(50);
+
+// let arc = arcGenerator({
+//   startAngle: 0,
+//   endAngle: 2 * Math.PI
+// });
+
+// d3.select('#projects-pie-plot')
+//   .append('path')
+//   .attr('d', arc)
+//   .attr('fill', 'red');
+
+
+// Step 1.4 (Step 2.1)
+// let data = [
+//   { value: 1, label: 'apples' },
+//   { value: 2, label: 'oranges' },
+//   { value: 3, label: 'mangos' },
+//   { value: 4, label: 'pears' },
+//   { value: 5, label: 'limes' },
+//   { value: 5, label: 'cherries' },
+// ];
+
+
+// Step 3
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year
+);
+
+let data = rolledData.map(([year, count]) => {
+  return {
+    value: count,
+    label: year
+  };
+});
+
+let arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(50);
+
+let sliceGenerator = d3.pie().value((d) => d.value);
+
+let arcData = sliceGenerator(data);
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, idx) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+});
+
+
+// Step 2.2
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+});
